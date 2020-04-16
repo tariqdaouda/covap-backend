a035d6573af63942a55d7f63d72c40b4d5341585
tariqdaouda <tariq@sauropod.com>
2020-04-15 21:34:31 -0400

    removed /api

--- api_exemples.sh
+++ api_exemples.sh
@@ -1,17 +1,17 @@
 echo "404"
-curl -i http://127.0.0.1:6543/api/tratra -X GET -H "Content-Type: application/json"
+curl -i http://127.0.0.1:6543/tratra -X GET -H "Content-Type: application/json"
 
 echo "Get fields, limit the enumeration lists to 20"
-curl -i http://127.0.0.1:6543/api/get-fields -X POST -H "Content-Type: application/json" --data '{"limit": 20}'
+curl -i http://127.0.0.1:6543/get-fields -X POST -H "Content-Type: application/json" --data '{"limit": 20}'
 
 echo "Get fields, limit the enumeration lists to 20"
-curl -i http://127.0.0.1:6543/api/get-fields/limit/20 -X GET -H "Content-Type: application/json"
+curl -i http://127.0.0.1:6543/get-fields/limit/20 -X GET -H "Content-Type: application/json"
 
 echo "Get fields for collection VirusSequences, limit the enumeration lists to 20"
-curl -i http://127.0.0.1:6543/api/get-fields/VirusSequences/limit/20 -X GET -H "Content-Type: application/json"
+curl -i http://127.0.0.1:6543/get-fields/VirusSequences/limit/20 -X GET -H "Content-Type: application/json"
 
 echo "Get values with join"
-curl -i http://127.0.0.1:6543/api/get-data -X POST -H "Content-Type: application/json" --data '{
+curl -i http://127.0.0.1:6543/get-data -X POST -H "Content-Type: application/json" --data '{
         "payload": {
             "query":{
                 "Peptides.Score": {
@@ -38,7 +38,7 @@ curl -i http://127.0.0.1:6543/api/get-data -X POST -H "Content-Type: application
     }'
 
 echo "Get values without join"
-curl -i http://127.0.0.1:6543/api/get-data -X POST -H "Content-Type: application/json" --data '{
+curl -i http://127.0.0.1:6543/get-data -X POST -H "Content-Type: application/json" --data '{
         "payload": {
             "query":{
                 "Peptides.Score": {
@@ -56,7 +56,7 @@ curl -i http://127.0.0.1:6543/api/get-data -X POST -H "Content-Type: application
     }'
 
 echo "Get peptides with a score >=0.7"
-curl -i http://127.0.0.1:6543/api/get-data -X POST -H "Content-Type: application/json" --data '{
+curl -i http://127.0.0.1:6543/get-data -X POST -H "Content-Type: application/json" --data '{
         "payload": {
             "query":{
                 "Peptides.Score": {
@@ -74,7 +74,7 @@ curl -i http://127.0.0.1:6543/api/get-data -X POST -H "Content-Type: application
     }'
 
 echo "Get peptides with a index >=10"
-curl -i http://127.0.0.1:6543/api/get-data -X POST -H "Content-Type: application/json" --data '{
+curl -i http://127.0.0.1:6543/get-data -X POST -H "Content-Type: application/json" --data '{
         "payload": {
             "query":{
                 "Peptides.Index": {
@@ -92,7 +92,7 @@ curl -i http://127.0.0.1:6543/api/get-data -X POST -H "Content-Type: application
     }'
 
 echo "Get peptides with a index >=10"
-curl -i http://127.0.0.1:6543/api/get-data -X POST -H "Content-Type: application/json" --data '{
+curl -i http://127.0.0.1:6543/get-data -X POST -H "Content-Type: application/json" --data '{
         "payload": {
                 "query":{
                     "Peptides.Score": {
